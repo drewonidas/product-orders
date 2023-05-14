6ddcc27c0c2253c01ac361dd80125b2a75761ec9
Andrew <drewonidas>
2023-05-14 13:10:58 +0200

    Initiated new spring project

---
+++ src/main/java/com/example/productorders/ProductOrdersController.java
@@ -0,0 +1,16 @@
+package com.example.productorders;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductOrdersController {
+    
+    @PostMapping("/orders")
+    public ResponseEntity<String> orderProduct(@RequestBody String orderRequest) {
+        
+        return ResponseEntity.ok(null);
+    }
+}
